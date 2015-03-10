51012fcf836090b2a29f2052447ba70b7eea556e
Nassim Ait boufrad <nassim.aitboufrad@francetv.fr>
2015-03-10 18:36:14 +0100

    correction backup

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -34,6 +34,7 @@ class mariadb::backup (
   database_user { "${backupuser}@localhost":
     ensure        => $ensure,
     password_hash => mysql_password($backuppassword),
+    require       => Class['mariadb::server'],
   }
 
   database_grant { "${backupuser}@localhost":
